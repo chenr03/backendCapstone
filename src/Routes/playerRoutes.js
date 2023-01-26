a7eaea8a564359f3e572bc5c1e530aa8952cfd99
Chris Henry <chrislouishenry@gmail.comgit statuscd~cd>
2023-01-26 10:48:20 -0600

    changed some things again to fix backend

--- src/Routes/playerRoutes.js
+++ src/Routes/playerRoutes.js
@@ -7,8 +7,8 @@ let controller = require("../Controller/players");
 let { checkJWT } = require('../middleware/authorization')
 
 // These routes are for my AllPlayers Table
-Routes.get("/players", controller.getPlayers)
-Routes.get("/player/:playerId", controller.getSinglePlayer);
+Routes.get("/players", controller.AllPlayers)
+Routes.get("/player/:playerId", controller.SinglePlayer);
 Routes.post('/player', checkJWT, controller.createPlayer);
 Routes.delete('/player/:playerId', checkJWT, controller.deletePlayer);
 Routes.put('/player/:playerId', checkJWT, controller.updatePlayer);
