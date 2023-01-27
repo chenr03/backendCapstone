8335be3262d279c3155c4efb7709dad4c5f0e8d3
Chris Henry <chrislouishenry@gmail.comgit statuscd~cd>
2023-01-26 20:07:57 -0600

    removed nodemon and changed to node

--- src/Routes/loginRoutes.js
+++ src/Routes/loginRoutes.js
@@ -10,14 +10,7 @@ let loginController = require("../Controller/loginController");
 // GET /hello
 
 router.get("/hello", loginController.hello);
-// only requests that have a valid token are allowed in
 
-// router.get("/admin", auths.checkJWT); //adminController.checkAdmin
-//
-// // GET /private hello
-// router.get("/privateHello", auths.checkJWT, loginController.privateHello);
-// // if logged in, do stuff
-// // if not fail
 if(loginController){
     console.log('Logged in 😀');
 } else {
