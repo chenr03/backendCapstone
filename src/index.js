1e961e0eacf2a43199557fb0f2027f01cff64c00
Chris Henry <chrislouishenry@gmail.comgit statuscd~cd>
2023-01-27 18:47:23 -0600

    switched login back to local host

--- src/index.js
+++ src/index.js
@@ -1,66 +1,46 @@
 let express = require('express');
-
 require("dotenv").config();
-
 let PORT = process.env.PORT || 8080;
-
 let discGolfApp = express();
 let cors = require('cors')
 
-
+// App is using Cors: //
 discGolfApp.use(cors())
 
-// message from backend
+// Message from backend
 discGolfApp.get("/", (req, res) => {
     res.json({
         message: "Welcome to the Community Disc Golf Server",
     });
 });
 
-
-
-
-
-
-
 // Packages that are being used on the discGolfApp:
 discGolfApp.use(express.json());
 discGolfApp.use(express.static("./backendCapstone/public"));
 
-
 // Authorization Routes and Login Routes that are being used on the discGolfApp:
-let authRoutes = require("./src/Routes/authorizationRoutes")
+let authRoutes = require("./Routes/authorizationRoutes")
 discGolfApp.use(authRoutes);
 
-let loginRoutes = require("./src/Routes/loginRoutes")
+let loginRoutes = require("./Routes/loginRoutes")
 discGolfApp.use(loginRoutes);
 
-// discGolfApp.use("/success", function(request, response){
-//     response.send("Deployed Successfully")
-// });
 
+// All Disc Golf Routes
 
-// I will capture all the Disc Golf Routes we have
-// in one place, instead of defining the Routes directly
-// on the app object
-
-let playerRoutes = require("./src/Routes/playerRoutes");
+let playerRoutes = require("./Routes/playerRoutes");
 discGolfApp.use(playerRoutes);
 
-let userRoutes = require("./src/Routes/userRoutes");
+let userRoutes = require("./Routes/userRoutes");
 discGolfApp.use(userRoutes);
 
-let gameRoutes = require("./src/Routes/gameRoutes");
+let gameRoutes = require("./Routes/gameRoutes");
 discGolfApp.use(gameRoutes);
 
-let courseRoutes = require("./src/Routes/courseRoutes");
+let courseRoutes = require("./Routes/courseRoutes");
 discGolfApp.use(courseRoutes);
 
-// only for users table and login
-
-// console.log(player_1_Routes)
-
-
+// Listening on the PORT: //
 discGolfApp.listen(PORT, function(){
     console.log('Application started on PORT', PORT);
 })
